b7ad214ca707ce5e356fd16fce6c6678723612b5
isabellaes <borjessonisabella@gmail.com>
2024-04-13 10:25:30 +0200

    updated reducer

--- src/context/Reducer.tsx
+++ src/context/Reducer.tsx
@@ -4,7 +4,12 @@ export const Reducer = (state: CartItem[], action: Action) => {
   switch (action.type) {
     case "ADD_CART_ITEM":
       return [...state, action.payload];
+      case "REMOVE_CART_ITEM":
+        return state.filter((i) => i.id != action.payload);
 
+    case "CLEAR_CART":
+      state = [];
+      return state;
     default:
       return state;
   }
