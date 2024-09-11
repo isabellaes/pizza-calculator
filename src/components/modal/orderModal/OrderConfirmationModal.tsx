1dcdcd964f491309c7f98c3c6e5044cfb763379b
isabellaes <borjessonisabella@gmail.com>
2024-09-11 13:44:04 +0200

    fix bug

--- src/components/modal/orderModal/OrderConfirmationModal.tsx
+++ src/components/modal/orderModal/OrderConfirmationModal.tsx
@@ -1,13 +1,29 @@
-import { CartItem } from "../../../utils/Types";
-
-type OrderConfirmationModalProps = {
-  items: CartItem[];
-};
+import { useContext } from "react";
+import { CartContext } from "../../../context/CartContextProvider";
 
 const OrderConfirmationModal = () => {
+  const { state } = useContext(CartContext);
+
+  function calculateTotal() {
+    return state.reduce((total, item) => {
+      return total + item.product.product.price * item.quantity;
+    }, 0);
+  }
+
   return (
     <>
       <h1>Thank you for your order!</h1>
+
+      <ul>
+        {state.map((x) => (
+          <>
+            <h2>{x.product.product.name}</h2>
+            <p>Quantity: {x.quantity}</p>
+            <p>Price: ${x.product.product.price}</p>
+          </>
+        ))}
+      </ul>
+      <h2>Total: ${calculateTotal()}</h2>
     </>
   );
 };
